b6f64318ac1cf248603907f2311ea4ca9cbc96ae
Luis-Domenech <luisfabiandomenech@gmail.com>
2022-08-22 03:47:00 -0400

    fix(scalar): Upgraded DecimalScalar implementation

--- packages/generator/src/helpers/genScalars.ts
+++ packages/generator/src/helpers/genScalars.ts
@@ -8,29 +8,24 @@ const indent = (index: number) => {
 export const genScalars = (models: string) => {
   if (contains(models, SCALARS)) {
     return (
-`export const DecimalScalar = new GraphQLScalarType({
-${indent(1)}name: "Decimal",
-${indent(1)}description: "GraphQL Scalar representing the Prisma.Decimal type, based on Decimal.js library.",
-${indent(1)}serialize: (value: unknown) => {
-${indent(2)}if (!(Prisma.Decimal.isDecimal(value))) {
-${indent(3)}throw new Error(\`[DecimalError] Invalid argument: \${Object.prototype.toString.call(value)}. Expected Prisma.Decimal.\`)
-${indent(2)}}
-${indent(2)}return (value as Prisma.Decimal).toString()
-${indent(1)}},
-${indent(1)}parseValue: (value: unknown) => {
-${indent(2)}if (!(typeof value === "string")) {
-${indent(3)}throw new Error(\`[DecimalError] Invalid argument: \${typeof value}. Expected string.\`)
-${indent(2)}}
-${indent(2)}return new Prisma.Decimal(value)
-${indent(1)}},
-${indent(1)}parseLiteral(ast) {
-${indent(2)}if (ast.kind !== Kind.STRING) {
-${indent(3)}throw new Error("StringOrInt can only parse string or number values")
-${indent(2)}}
-${indent(2)}return new Prisma.Decimal(ast.value)
-${indent(1)}}
-})
-`
+      [
+        `export const DecimalScalar = new GraphQLScalarType({`,
+        `$indent(1)}name: 'Decimal',`,
+        `$indent(1)}description: 'GraphQL Scalar representing the Prisma.Decimal type, based on Decimal.js library.',`,
+        `$indent(1)}serialize: (value: unknown) => {`,
+        `$indent(1)}if (!Prisma.Decimal.isDecimal(value)) throw new Error(\`[DecimalError] Invalid argument: \${Object.prototype.toString.call(value)}. Expected Prisma.Decimal.\`)`,
+        `$indent(1)}return (value as Prisma.Decimal).toString()`,
+        `$indent(1)}},`,
+        `$indent(1)}parseValue: (value: unknown) => {`,
+        `$indent(1)}if (typeof value !== 'string' && typeof value !== "number") throw new Error(\`[DecimalError] Invalid argument: \${typeof value}. Expected string.\`)`,
+        `$indent(1)}return new Prisma.Decimal(value)`,
+        `$indent(1)}},`,
+        `$indent(1)}parseLiteral(ast) {`,
+        `$indent(1)}if (ast.kind !== Kind.STRING && ast.kind !== Kind.INT && ast.kind !== Kind.FLOAT) throw new Error('DecimalScalar can only parse string, int, float and object values')`,
+        `$indent(1)}return new Prisma.Decimal(ast.value)`,
+        `$indent(1)}},`,
+        `})`
+      ].join("\n")
     )
   }
 }
